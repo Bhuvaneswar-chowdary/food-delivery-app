22608850150b74f34ba979e24ba6e29e3b4a6649
lokesh <nallalokesh23@gmail.com>
2025-04-02 00:17:21 +0530

    updated css

--- react-pro/src/components/Fooditem.jsx
+++ react-pro/src/components/Fooditem.jsx
@@ -22,8 +22,6 @@ const Fooditem = ({ _id, name, price, description, image }) => {
     <div className="food-item">
       <div className="food-item-img container">
         <img src={updatedImage} alt="" className="food-item-img" />
-
-
       </div>
       {
         !cartitems[_id]
@@ -33,7 +31,6 @@ const Fooditem = ({ _id, name, price, description, image }) => {
           }}>
             <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
           </svg>
-
           :
           <div className="food-item-counter">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16" onClick={() => {
@@ -48,22 +45,15 @@ const Fooditem = ({ _id, name, price, description, image }) => {
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-dash-lg" viewBox="0 0 16 16" onClick={() => removefromcart(_id)}>
               <path fillRule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8" />
             </svg>
-
-
-
-
           </div>
-
       }
       <div className="food-item-info">
         <div className="food-item-rating">
           <p>{name}</p>
-
-
           <img src={rating} alt="" />
         </div>
         <p className="food-item-description">{description}</p>
-        <p className="food-item-price">{price}</p>
+        <p className="food-item-price">₹{price}</p>
       </div>
     </div>
   )
